cddfcf4e4d3fc2a40b3595a1c7ef2e45181e25e0
nothing19special <meow1936meow@gmail.com>
2026-08-25 14:58:58 +0300

    Начало выполнения задания по параметризации

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,4 +46,3 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
 
 
 
-
---
+++ tests/test_authorization.py
@@ -0,0 +1,23 @@
+from playwright.sync_api import sync_playwright, expect, Page
+import pytest
+
+@pytest.mark.regression
+@pytest.mark.authorization
+@pytest.mark.parametrize('email', ['user.name@gmail.com','user.name@gmail.com', '  '])
+@pytest.mark.parametrize('password', ['password','  ', 'password'])
+def test_wrong_email_or_password_authorization(chromium_page_with_state: Page, email: str, password: str):
+    # Все остальные действия остаются без изменений
+    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+
+    email_input = chromium_page_with_state.get_by_test_id('login-form-email-input').locator('input')
+    email_input.fill(email)
+
+    password_input = chromium_page_with_state.get_by_test_id('login-form-password-input').locator('input')
+    password_input.fill(password)
+
+    login_button = chromium_page_with_state.get_by_test_id('login-page-login-button')
+    login_button.click()
+
+    wrong_email_or_password_alert = chromium_page_with_state.get_by_test_id('login-page-wrong-email-or-password-alert')
+    expect(wrong_email_or_password_alert).to_be_visible()
+    expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
