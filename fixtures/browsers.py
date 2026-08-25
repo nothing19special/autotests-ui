7f0bb7f91a7dd2822f8d0ebeb38ebcfb952193d2
nothing19special <meow1936meow@gmail.com>
2026-08-25 22:33:12 +0300

    6.7 Параметризация Pytest. Практика работы с pytest параметризацией

---
+++ fixtures/browsers.py
@@ -0,0 +1,56 @@
+import pytest
+from playwright.sync_api import sync_playwright, Page, Playwright
+
+
+#Открытие страницы. Без дополнительных параметров
+@pytest.fixture(scope='session')
+def chromium_page(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
+
+
+#Создание пользака для получения данных авторизации
+@pytest.fixture(scope='session')
+def initialize_browser_state(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    # Переход на страницу регистрации
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    # Ввод почты
+    registration_email_form = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_email_form.fill('email@test.test')
+
+    # Ввод никнейма
+    registration_username_form = page.get_by_test_id('registration-form-username-input').locator('input')
+    registration_username_form.fill('username')
+
+    # Ввод пароля
+    registration_password_form = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_password_form.fill('password')
+
+    # Нажатия на кнопку регистрации
+    registration_button_click = page.get_by_test_id('registration-page-registration-button')
+    registration_button_click.click()
+
+    # Сохранения состояния клиента
+    context.storage_state(path='browser-state.json')
+
+#Создание страницы с новым контекстом
+@pytest.fixture(scope='function')
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    #Возвращение страницы
+    yield page
+    #Закрытие браузера (опционально)
+    browser.close()
+
+
+
+
--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -3,21 +3,20 @@ import pytest
 
 @pytest.mark.regression
 @pytest.mark.authorization
-@pytest.mark.parametrize('email', ['user.name@gmail.com','user.name@gmail.com', '  '])
-@pytest.mark.parametrize('password', ['password','  ', 'password'])
-def test_wrong_email_or_password_authorization(chromium_page_with_state: Page, email: str, password: str):
+@pytest.mark.parametrize('email, password', [('user.name@gmail.com', 'password',), ('user.name@gmail.com', '  '),('  ', 'password')])
+def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
     # Все остальные действия остаются без изменений
-    chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    email_input = chromium_page_with_state.get_by_test_id('login-form-email-input').locator('input')
+    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill(email)
 
-    password_input = chromium_page_with_state.get_by_test_id('login-form-password-input').locator('input')
+    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
     password_input.fill(password)
 
-    login_button = chromium_page_with_state.get_by_test_id('login-page-login-button')
+    login_button = chromium_page.get_by_test_id('login-page-login-button')
     login_button.click()
 
-    wrong_email_or_password_alert = chromium_page_with_state.get_by_test_id('login-page-wrong-email-or-password-alert')
+    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible()
     expect(wrong_email_or_password_alert).to_have_text("Wrong email or password")
